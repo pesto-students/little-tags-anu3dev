9e16a42f38868624fe489462705592b49c0d37dd
anu3dev <anu3dev@gmail.com>
2021-04-09 23:34:17 +0530

    user and signup login modal

--- src/static/subbar/SubBar.jsx
+++ src/static/subbar/SubBar.jsx
@@ -19,10 +19,53 @@ export default function SubBar(){
                 </div>
                 <div className="subBarRightTwo">
                     <ul>
-                        <li><a href="."><i className="las la-user"></i></a></li>
+                        <li><a href="." data-toggle="modal" data-target="#myModal"><i className="las la-user"></i></a></li>
                         <li><a href="."><i className="las la-heart"></i></a></li>
                         <li><a href="."><i className="las la-shopping-cart"></i></a></li>
                     </ul>
+                </div>
+            </div>
+            <div className="modal" id="myModal">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title">Welcome Back!</h1>
+                            <a href="." data-dismiss="modal"><i className="las la-times"></i></a>
+                        </div>
+                        <div className="modal-body">
+                            <button type="button" className="btn1 btnWidth">Login with facebook</button><br/>
+                            <button type="button" className="btn1 btnWidth" >Login with google</button><br/>
+                            <input type="text" placeholder="Enter Email" /><br/>
+                            <input type="text" placeholder="Enter Password" /><br/>
+                            <button type="button" className="btn1 btnWidth" >Submit</button>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn1 btnFoot" data-toggle="modal" data-target="#myModal1">SignUp</button>
+                            <button type="button" className="btn1 btnFoot" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal" id="myModal1">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title">It's first time only!</h1>
+                            <a href="." data-dismiss="modal"><i className="las la-times"></i></a>
+                        </div>
+                        <div className="modal-body">
+                            <button type="button" className="btn1 btnWidth">SignUp with facebook</button><br/>
+                            <button type="button" className="btn1 btnWidth" >SignUp with google</button><br/>
+                            <input type="text" placeholder="Enter Full Name" />
+                            <input type="text" placeholder="Enter Email" /><br/>
+                            <input type="text" placeholder="Enter Password" /><br/>
+                            <button type="button" className="btn1 btnWidth" >Submit</button>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn1 btnFoot" data-toggle="modal" data-target="#myModal1">Login</button>
+                            <button type="button" className="btn1 btnFoot" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

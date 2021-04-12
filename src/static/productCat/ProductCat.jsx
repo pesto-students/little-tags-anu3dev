926025f83328532a67a5efe5836b1cb29c80a4ce
anu3dev <anu3dev@gmail.com>
2021-04-13 01:13:44 +0530

    image add

--- src/static/productCat/ProductCat.jsx
+++ src/static/productCat/ProductCat.jsx
@@ -15,50 +15,50 @@ export default function ProductCat() {
       <h1>Product Categories</h1>
       <div className="row">
         <div className="productCatTop col-lg-6 col-md-6">
-          <img src="/images/MensClothing_1.jpg" alt="men" />
-          <p className="onTextButton">Styles for Men</p>
+          <img src="/img/proD.png" alt="men" />
+          <p className="onTextButton"></p>
           <Link
             className="onImageButton btn"
             to={"/productsList/" + MENS_CLOTHING}
           >
-            Browse Products
+            Indian Wear
           </Link>
         </div>
         <div className="productCatTop col-lg-6 col-md-6">
-          <img src="/images/Jewellery_1.jpg" alt="jewellery" />
-          <p className="onTextButton">Jewellery</p>
+          <img src="/img/proE.png" alt="jewellery" />
+          <p className="onTextButton"></p>
           <Link className="onImageButton btn" to={"/productsList/" + JEWELLERY}>
-            Browse Products
+          Western Wear
           </Link>
         </div>
         <div className="productCatBot col-lg-4 col-md-4">
-          <img src="/images/Electronics_1.png" alt="" />
-          <p className="onTextButtonSmall">Electronics</p>
+          <img src="/img/proA.png" alt="" />
+          <p className="onTextButtonSmall"></p>
           <Link
             className="onImageButtonSmall btn"
             to={"/productsList/" + ELECTRONICS}
           >
-            Browse Products
+            Beauty Care
           </Link>
         </div>
         <div className="productCatBot col-lg-4 col-md-4">
-          <img src="/images/Womensclothing_1.jpg" alt="" />
-          <p className="onTextButtonSmall">Style for Women</p>
+          <img src="/img/proB.png" alt="" />
+          <p className="onTextButtonSmall"></p>
           <Link
             className="onImageButtonSmall btn"
             to={"/productsList/" + WOMENS_CLOTHING}
           >
-            Browse Products
+            Jewellery
           </Link>
         </div>
         <div className="productCatBot col-lg-4 col-md-4">
-          <img src="/images/Kids_1.jpg" alt="" />
-          <p className="onTextButtonSmall">Kids</p>
+          <img src="/img/proC.png" alt="" />
+          <p className="onTextButtonSmall"></p>
           <Link
             className="onImageButtonSmall btn"
             to={"/productsList/" + KIDS_CLOTHING}
           >
-            Browse Products
+            Electronics
           </Link>
         </div>
       </div>

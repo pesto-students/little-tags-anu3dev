03fd7514e9facd8090a8045575303377f00ce7db
LakshmiPrabha <67004947+lakshmiprabhat@users.noreply.github.com>
2021-04-13 09:37:10 +0530

    Provide links to Menu bar list and update images in Public folder

--- src/static/menubar/MenuBar.jsx
+++ src/static/menubar/MenuBar.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import "./MenuBar.scss";
+import { Link } from "react-router-dom";
+import {
+  MENS_CLOTHING,
+  WOMENS_CLOTHING,
+  JEWELLERY,
+  ELECTRONICS,
+} from "../../components/common/Routes";
 
 export default function MenuBar() {
   return (
     <div className="menuBar">
       <ul>
         <li>
-          <a href="."><i className="las la-bars"></i></a>
+          <a href=".">
+            <i className="las la-bars"></i>
+          </a>
         </li>
-        <li><a href=".">Mens Wear</a></li>
-        <li><a href=".">Womens Wear</a></li>
-        <li><a href=".">Beauty Care</a></li>
-        <li><a href=".">Jewellery</a></li>
-        <li><a href=".">Electronics</a></li>
+        <li>
+          <Link to={"/productsList/" + MENS_CLOTHING}>Mens Wear</Link>
+        </li>
+        <li>
+          <Link to={"/productsList/" + WOMENS_CLOTHING} href=".">
+            Womens Wear
+          </Link>
+        </li>
+        <li>
+          <Link to={"/productsList/" + JEWELLERY}>Beauty Care</Link>
+        </li>
+        <li>
+          <Link to={"/productsList/" + JEWELLERY}>Jewellery</Link>
+        </li>
+        <li>
+          <Link to={"/productsList/" + ELECTRONICS}>Electronics</Link>
+        </li>
       </ul>
     </div>
   );

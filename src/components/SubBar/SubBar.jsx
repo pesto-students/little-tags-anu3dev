45058cd0aaa0e89c001c7fee54408e14d736f43b
LakshmiPrabha <67004947+lakshmiprabhat@users.noreply.github.com>
2021-04-17 16:44:47 +0530

    Add cartcount to cartlink

--- src/components/SubBar/SubBar.jsx
+++ src/components/SubBar/SubBar.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import "./SubBar.scss";
 import { Link } from "react-router-dom";
 import * as ROUTES from "../common/Routes";
-import Modal from "../Modal/Modal"
+import Modal from "../Modal/Modal";
+import { useSelector } from "react-redux";
 
 export default function SubBar() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.quantity) + qty, 0);
+  };
   return (
     <div className="subBar">
       <div className="subBarLeft">
@@ -23,35 +29,65 @@ export default function SubBar() {
         <div className="subBarRightTwo">
           <ul>
             <li>
-              <a href=".">
-                <i className="las la-shopping-cart"></i>
-              <span>Cart</span> </a>
+              <Link to={"/cart"}>
+                <a href=".">
+                  <i className="las la-shopping-cart"></i>
+                  <span>Cart({getCartCount()})</span>
+                </a>
+              </Link>
             </li>
             <li>
-            <div className="dropdown">
-              <a href="." className="dropdown-toggle" data-toggle="dropdown" >
-                <i className="las la-user"></i>
-                <span>Login</span>
-              </a>
+              <div className="dropdown">
+                <a href="." className="dropdown-toggle" data-toggle="dropdown">
+                  <i className="las la-user"></i>
+                  <span>Login</span>
+                </a>
                 <div className="dropdown-menu">
                   <hr className="hrLine"></hr>
-                  <Link to={ROUTES.ACCOUNT} className="dropdown-item" ><i className="las la-user-alt"></i> Your Account</Link>
-                  <Link to={ROUTES.ORDER} className="dropdown-item"><i className="las la-gift"></i> Your Order</Link>
-                  <Link to={ROUTES.WISHLIST} className="dropdown-item"><i className="las la-heart"></i> Wishlist</Link>
+                  <Link to={ROUTES.ACCOUNT} className="dropdown-item">
+                    <i className="las la-user-alt"></i> Your Account
+                  </Link>
+                  <Link to={ROUTES.ORDER} className="dropdown-item">
+                    <i className="las la-gift"></i> Your Order
+                  </Link>
+                  <Link to={ROUTES.WISHLIST} className="dropdown-item">
+                    <i className="las la-heart"></i> Wishlist
+                  </Link>
                   <hr className="hrLine"></hr>
                   <p className="dropdownText">If you are a new user</p>
                   <div className="btnMid">
-                    <a href="." data-toggle="modal" data-target="#myModal1">Signup</a>
+                    <a href="." data-toggle="modal" data-target="#myModal1">
+                      Signup
+                    </a>
                   </div>
                   <div className="btnMid">
-                    <a href="." data-toggle="modal" data-target="#myModal" className="btn">Login</a>
+                    <a
+                      href="."
+                      data-toggle="modal"
+                      data-target="#myModal"
+                      className="btn"
+                    >
+                      Login
+                    </a>
                   </div>
                   <hr className="hrLine"></hr>
                   <p className="dropdownText">With social links</p>
                   <ul>
-                    <li><a href="."><i className="lab la-google-plus-g"></i></a></li>
-                    <li><a href="."><i className="lab la-facebook"></i></a></li>
-                    <li><a href="."><i className="lab la-linkedin-in"></i></a></li>
+                    <li>
+                      <a href=".">
+                        <i className="lab la-google-plus-g"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href=".">
+                        <i className="lab la-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href=".">
+                        <i className="lab la-linkedin-in"></i>
+                      </a>
+                    </li>
                   </ul>
                 </div>
               </div>

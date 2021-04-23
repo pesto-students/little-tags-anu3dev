ff77df5bc65dbca6e399a5571d16d817efbefb94
LakshmiPrabha <67004947+lakshmiprabhat@users.noreply.github.com>
2021-04-24 00:08:13 +0530

    Remove localstorage for cartkey in firebase.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,7 +1,6 @@
 import app from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
-import { getDataFromLocalStorage, setDataToLocalStorage } from "../common/Util";
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
